6c26bb0773ce22e2281600f5085f9898a15bf58d
Christos Kormaris <chriskormaris@gmail.com>
2019-10-04 01:52:17 +0300

    -Comment out the means, variance and standard deviation implementation that expand the bounding box's borders. Leave the borders as they are, narrow. -Change initial values of minLongitude to 180, maxLongitude to -180. -Rename the List<LatLong> object "quadrangle" to "boundingBox".

--- src/plot/Utilities.java
+++ src/plot/Utilities.java
@@ -98,12 +98,4 @@ public class Utilities {
 		return Math.sqrt(sum);
 	}
 	
-	public static double euclideanNorm(double[] x) {
-		double sum = 0;
-		for (int i=0; i<x.length; i++) {
-			sum += Math.pow(x[i], 2);
-		}
-		return Math.sqrt(sum);
-	}
-	
 }
